8dd25f490cf4c09b4a254fd8f24990268794ddc4
Martin Lambers <marlam@marlam.de>
2020-11-06 15:57:55 +0100

    Move all keyboard shortcuts to QActions

--- qv.hpp
+++ qv.hpp
@@ -97,7 +97,6 @@ public:
     virtual void paintGL() override;
     virtual void resizeGL(int w, int h) override;
 
-    virtual void keyPressEvent(QKeyEvent* e) override;
     virtual void mouseMoveEvent(QMouseEvent* e) override;
     virtual void mousePressEvent(QMouseEvent* e) override;
     virtual void mouseReleaseEvent(QMouseEvent* e) override;

2bfe8a3861f96d026ab08a17050a3c853b1a2c94
Martin Lambers <marlam@marlam.de>
2021-05-28 11:23:23 +0200

    Adjust default brightness for dynamic range reduction.

--- parameters.cpp
+++ parameters.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2019 Computer Graphics Group, University of Siegen
+ * Copyright (C) 2019, 2020, 2021
+ * Computer Graphics Group, University of Siegen
  * Written by Martin Lambers <martin.lambers@uni-siegen.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,7 +33,7 @@ Parameters::Parameters() :
     _colorVisMaxVal(std::numeric_limits<float>::quiet_NaN()),
     magInterpolation(true), magGrid(false),
     zoom(1.0f), xOffset(0), yOffset(0),
-    dynamicRangeReduction(false), drrBrightness(64.0f)
+    dynamicRangeReduction(false), drrBrightness(32.0f)
 {
 }
 

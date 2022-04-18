0b0f6a58c97b92873e7ffddb3a17a0dc41e20482
Martin Lambers <marlam@marlam.de>
2022-04-18 14:11:32 +0200

    Fix version number in fallback overlay

--- overlay-fallback.cpp
+++ overlay-fallback.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2019, 2020 Computer Graphics Group, University of Siegen
+ * Copyright (C) 2019, 2020, 2021, 2022
+ * Computer Graphics Group, University of Siegen
  * Written by Martin Lambers <martin.lambers@uni-siegen.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -30,7 +31,7 @@
 
 OverlayFallback::OverlayFallback()
 {
-    _fallbackText << "         qv 4.0";
+    _fallbackText << "         qv " QV_VERSION;
     _fallbackText << "";
     _fallbackText << "  https://marlam.de/qv";
 }
